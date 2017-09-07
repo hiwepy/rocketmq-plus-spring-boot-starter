bc5d6b2d3b058023fe2b42e8117a7a3fe668a3d4
vindell <hnxyhcwdl1003@163.com>
2017-09-07 18:25:35 +0800

    临时提交

--- src/main/java/org/apache/rocketmq/spring/boot/handler/impl/RocketmqEventMessageHandler.java
+++ src/main/java/org/apache/rocketmq/spring/boot/handler/impl/RocketmqEventMessageHandler.java
@@ -2,15 +2,14 @@ package org.apache.rocketmq.spring.boot.handler.impl;
 
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.common.message.MessageExt;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 import org.apache.rocketmq.spring.boot.event.RocketmqEvent;
 import org.apache.rocketmq.spring.boot.handler.AbstractRouteableMessageHandler;
 import org.apache.rocketmq.spring.boot.handler.MessageHandler;
-import org.apache.rocketmq.spring.boot.handler.chain.HandlerChain;
 import org.apache.rocketmq.spring.boot.handler.chain.HandlerChainResolver;
+import org.apache.rocketmq.spring.boot.handler.chain.OrgiHandlerChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Component
 public class RocketmqEventMessageHandler extends AbstractRouteableMessageHandler<RocketmqEvent> implements MessageHandler {
@@ -24,17 +23,8 @@ public class RocketmqEventMessageHandler extends AbstractRouteableMessageHandler
 	@Override
 	public boolean handleMessage(MessageExt msgExt, ConsumeConcurrentlyContext context) throws Exception {
 		try {
-			
-			HandlerChain<RocketmqEvent> originalChain = new HandlerChain<RocketmqEvent>() {
-				
-				@Override
-				public void doHandler(RocketmqEvent event) throws Exception {
-					
-				}
-				
-			};
 			//执行事件处理链
-			this.doHandler(new RocketmqEvent(msgExt), originalChain);
+			this.doHandler(new RocketmqEvent(msgExt), new OrgiHandlerChain());
 			return true;
 		} catch (Exception e) {
 			LOG.error(e.getMessage());
